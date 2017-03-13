c4220dd2f3a31e0e68c65d19dd25ad855ca248d1
Sascha Meiers <meiers@embl.de>
2017-03-13 16:39:24 +0100

    Changed how BAMs are read + cosmetic changes; (version might not run)

--- src/main.cpp
+++ src/main.cpp
@@ -38,6 +38,9 @@ Contact: Sascha Meiers (meiers@embl.de)
        - for the even further future: different SMs allowed.
 */
 
+static unsigned MIN_MEDIAN_PER_SAMPLE = 20;
+static double   MIN_MEDIAN_PER_BIN    = 0.1;
+
 int main(int argc, char **argv)
 {
 
@@ -46,10 +49,10 @@ int main(int argc, char **argv)
     boost::program_options::options_description generic("Generic options");
     generic.add_options()
         ("help,?", "show help message")
-        ("mapq,q", boost::program_options::value<int>(&conf.minMapQual)->default_value(50), "min mapping quality")
+        ("mapq,q", boost::program_options::value<int>(&conf.minMapQual)->default_value(10), "min mapping quality")
         ("window,w", boost::program_options::value<unsigned int>(&conf.window)->default_value(1000000), "window size")
         ("out,o", boost::program_options::value<boost::filesystem::path>(&conf.f_out)->default_value("out.txt"), "output file for counts")
-        ("mode,m", boost::program_options::value<std::string>(&conf.mode)->default_value("all"), "what to compute (raw|norm)")
+        ("mode,m", boost::program_options::value<std::string>(&conf.mode)->default_value("count"), "what to compute (count|classify)")
         ;
 
     boost::program_options::options_description hidden("Hidden options");
@@ -71,50 +74,54 @@ int main(int argc, char **argv)
 
 
     // Check command line arguments
-    if ((vm.count("help")) || (!vm.count("input-file")) || !(conf.mode == "norm" || conf.mode == "raw")) {
+    if ((vm.count("help")) || (!vm.count("input-file")) || !(conf.mode == "count" || conf.mode == "classify")) {
         std::cout << "Usage: " << argv[0] << " [OPTIONS] <strand.seq1.bam> <strand.seq2.bam> ... <strand.seqN.bam>" << std::endl;
         std::cout << visible_options << "\n";
         return 1;
     } 
 
 
-    // Open all bam files
-    typedef std::vector<samFile*> TSamFile;
-    typedef std::vector<hts_idx_t*> TIndex;    
-    TSamFile samfile;
-    TIndex idx;
-    samfile.resize(conf.f_in.size());
-    idx.resize(conf.f_in.size());
-    for(unsigned i = 0; i < conf.f_in.size(); ++i) {
-        samfile[i] = sam_open(conf.f_in[i].string().c_str(), "r");
-        if (samfile[i ] == NULL) {
-            std::cerr << "Fail to open file " << conf.f_in[i].string() << std::endl;
-            return 1;
-        }
-        idx[i] = sam_index_load(samfile[i], conf.f_in[i].string().c_str());
-        if (idx[i] == NULL) {
-            std::cerr << "Fail to open index for " << conf.f_in[i].string() << std::endl;
-            return 1;
-        }
-    }
+
 
     // Count in bins
-    bam_hdr_t* hdr = sam_hdr_read(samfile[0]);
     typedef std::vector<std::vector<Counter> > TGenomeCounts;
     std::vector<TGenomeCounts> counts;
     counts.resize(conf.f_in.size());
+    // Keep one header
+    bam_hdr_t* hdr;
+
     for(unsigned i = 0; i < counts.size(); ++i) {
+
+
+        // Open bam file
+        samFile* samfile = sam_open(conf.f_in[i].string().c_str(), "r");
+        if (samfile == NULL) {
+            std::cerr << "Fail to open file " << conf.f_in[i].string() << std::endl;
+            return 1;
+        }
+        hts_idx_t* idx = sam_index_load(samfile, conf.f_in[i].string().c_str());
+        if (idx == NULL) {
+            std::cerr << "Fail to open index for " << conf.f_in[i].string() << std::endl;
+            return 1;
+        }
+        // for now: keep just one single header for all
+        if (i==0)
+            hdr = sam_hdr_read(samfile);
+
+
         std::cout << "Reading " << conf.f_in[i].string() << std::endl;
         counts[i].resize(hdr->n_targets);
         for (int chrom = 0; chrom < hdr->n_targets; ++chrom) {
-            if (hdr->target_len[chrom] < conf.window) continue;
+
+            if (hdr->target_len[chrom] < conf.window)
+                continue;
             int bins = hdr->target_len[chrom] / conf.window + 1;
             std::vector<Counter> & counter = counts[i][chrom];
             counter.resize(bins, Counter());
 
-            hts_itr_t* iter = sam_itr_queryi(idx[i], chrom, 0, hdr->target_len[chrom]);
+            hts_itr_t* iter = sam_itr_queryi(idx, chrom, 0, hdr->target_len[chrom]);
             bam1_t* rec = bam_init1();
-            while (sam_itr_next(samfile[i], iter, rec) >= 0) {
+            while (sam_itr_next(samfile, iter, rec) >= 0) {
                 if (rec->core.flag & (BAM_FSECONDARY | BAM_FQCFAIL | BAM_FDUP | BAM_FSUPPLEMENTARY | BAM_FUNMAP))
                     continue;
                 if ((rec->core.qual < conf.minMapQual) || (rec->core.tid<0)) 
@@ -136,26 +143,26 @@ int main(int argc, char **argv)
             bam_destroy1(rec);
             hts_itr_destroy(iter);
         }
+
+        hts_idx_destroy(idx);
+        sam_close(samfile);
     }
-    
-    // Close bam files
-    for(unsigned i = 0; i < conf.f_in.size(); ++i) {
-        hts_idx_destroy(idx[i]);
-        sam_close(samfile[i]);
-    }
- 
+
+
  
     // mode "raw": print raw counts
-    if (conf.mode == "raw") {
+    if (conf.mode == "count")
+    {
+        std::cout << "Writing file " << conf.f_out.string() << std::endl;
         std::ofstream out(conf.f_out.string());
         out << "chrom\tstart\tend\tsample\tw\tc" << std::endl;
         for(unsigned i = 0; i < counts.size(); ++i) {
             for (unsigned chrom = 0; chrom < counts[0].size(); ++chrom) {
                 for (unsigned bin = 0; bin < counts[0][chrom].size(); ++bin) {
-                    Counter & cc = counts[0][chrom][bin];
+                    Counter & cc = counts[i][chrom][bin];
                     out << hdr->target_name[chrom];
                     out << "\t" << bin*conf.window << "\t" << (bin+1)*conf.window;
-                    out << "\t" << conf.f_in[i].string();
+                    out << "\t" << conf.f_in[i].filename().string();
                     out << "\t" << cc.watson_count;
                     out << "\t" << cc.crick_count;
                     out << std::endl;
@@ -169,69 +176,59 @@ int main(int argc, char **argv)
    
     // Normalize by sample:
     for(unsigned i = 0; i < counts.size(); ++i) {
-        
+
+        // calculate median count per sample:
         TMedianAccumulator<unsigned int> med_acc;
-        for (std::vector<Counter> & count_chrom : counts[i])
-            for(Counter & count_bin : count_chrom)
+        for (std::vector<Counter> const & count_chrom : counts[i])
+            for(Counter const & count_bin : count_chrom)
                 med_acc(count_bin.watson_count + count_bin.crick_count);
-        unsigned int sample_median = boost::accumulators::median(med_acc);
+        unsigned sample_median = boost::accumulators::median(med_acc);
         
-        if (sample_median < 20) {
-            std::cout << "Ignoring " << conf.f_in[i].string() << " due to too few reads. Consider increasing window size." << std::endl;
+        if (sample_median < MIN_MEDIAN_PER_SAMPLE) {
+            std::cout << "    Ignoring " << conf.f_in[i].string() << " due to too few reads. Consider increasing window size." << std::endl;
             continue;
         }
         
         for (std::vector<Counter> & count_chrom : counts[i]) {
             for(Counter & cc : count_chrom) {
-                cc.watson_norm = cc.watson_count / (double)sample_median;
-                cc.crick_norm  = cc.crick_count  / (double)sample_median;
+                cc.watson_norm = cc.watson_count / (double)sample_median * 2;
+                cc.crick_norm  = cc.crick_count  / (double)sample_median * 2;
             }
         }
     }
 
-
-
     // To do: How to handle samples that are kicked out?
 
-    
-    // Calculate median per bin:
+
+
+    // Blacklisting bins:
+    // - if median count in this bin is too low
+
+    // Calculate median per bin
     std::vector<std::vector<double> > bin_medians(counts[0].size());
     for (unsigned chrom = 0; chrom < counts[0].size(); ++chrom)
         bin_medians[chrom] = std::vector<double>(counts[0][chrom].size(), 1);
     
-    if (counts.size() >= 3) {
-        for (unsigned chrom = 0; chrom < counts[0].size(); ++chrom) {
-            if (counts[0][chrom].size() < 1) continue;
-            for (unsigned bin = 0; bin < counts[0][chrom].size(); ++bin) {
-                
-                TMedianAccumulator<double> med_acc;
-                for(unsigned i = 0; i < counts.size(); ++i) {
-                    Counter & cc = counts[i][chrom][bin];
-                    med_acc(cc.watson_norm + cc.crick_norm);
-                }
-                bin_medians[chrom][bin] = boost::accumulators::median(med_acc);
-            }
-        }
-    } else {
-        std::cout << "Normalizaiton per bin is skipped: too few sample" << std::endl;
-    }
-    
-    
-    // Blacklist bins with too low read count:
     for (unsigned chrom = 0; chrom < counts[0].size(); ++chrom) {
+        if (counts[0][chrom].size() < 1) continue;
         for (unsigned bin = 0; bin < counts[0][chrom].size(); ++bin) {
-            if (bin_medians[chrom][bin] < 0.1)
-                std::cout << "Black list chrom_id " << chrom << " bin " << bin << std::endl;
+            
+            TMedianAccumulator<double> med_acc;
+            for(unsigned i = 0; i < counts.size(); ++i) {
+                Counter & cc = counts[i][chrom][bin];
+                med_acc(cc.watson_norm + cc.crick_norm);
+            }
+            bin_medians[chrom][bin] = boost::accumulators::median(med_acc);
         }
     }
-    
-    // Normalize per bin
+
+    // Blacklist bins
     for(unsigned i = 0; i < counts.size(); ++i) {
         for (unsigned chrom = 0; chrom < counts[0].size(); ++chrom) {
             for (unsigned bin = 0; bin < counts[0][chrom].size(); ++bin) {
                 Counter & cc = counts[i][chrom][bin];
-                cc.watson_norm = cc.watson_norm / bin_medians[chrom][bin];
-                cc.crick_norm  = cc.crick_norm /  bin_medians[chrom][bin];
+                if (bin_medians[chrom][bin] < MIN_MEDIAN_PER_BIN)
+                    cc.label = "none";
             }
         }
     }
