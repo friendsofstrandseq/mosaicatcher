9fafd92a9fb39518a2a6d00117a406ee1cab0fc2
Sascha Meiers <meiers@embl.de>
2017-03-20 12:55:08 +0100

    plot other NB distribution

--- R/test_distributions.R
+++ R/test_distributions.R
@@ -1,17 +1,31 @@
 # Test distributions
 library(data.table)
 library(ggplot2)
-f = data.table(x = c(seq(100),
-                     seq(100),
-                     seq(100),
-                     seq(100)), 
-               y = c(dpois(seq(100),50), 
-                     dnbinom(seq(100),50,0.5),
-                     dnbinom(seq(100),50/3,0.25),
-                     dnbinom(seq(100),50/9,0.1)), 
-               type = c(rep("Poisson",100),
-                        rep("NB(50,0.5)",100),
-                        rep("NB(16.6,0.25)",100),
-                        rep("NB(5.5,0.1)",100)))
-ggplot(f) + aes(x,y,col=type) + geom_line() + theme_classic()
+MAX=30
+MEAN=10
+plot_NB <- function(mean, max) {
+    f = data.table(x = c(seq(max),
+                         seq(max),
+                         seq(max),
+                         seq(max),
+                         seq(max)),
+                   y = c(dpois(seq(max),mean),
+                         dnbinom(seq(max),mean,0.5),
+                         dnbinom(seq(max),mean/4,0.2),
+                         dnbinom(seq(max),mean/9,0.1),
+                         dnbinom(seq(max),mean/49,0.02)),
+                   type = c(rep("Poisson",max),
+                            rep("NB(p=0.5)",max),
+                            rep("NB(p=0.2)",max),
+                            rep("NB(p=0.1)",max),
+                            rep("NB(p=0.02)",max)))
+    plt <- ggplot(f) +
+        aes(x,y,col=type) +
+        geom_line() +
+        ggtitle(paste("Mean",mean)) +
+        theme_classic()
+    return(plt)
+}
 
+plot_NB(1200,2000)
+plot_NB(10,20)
