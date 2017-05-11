65440b1a99f3c0c425a4b28e8666c0d8b1c548ed
Sascha Meiers <meiers@embl.de>
2017-05-11 08:52:00 +0200

    fixed ideogram plots

--- R/qc.R
+++ R/qc.R
@@ -35,17 +35,18 @@ for (s in unique(d$sample))
         reads_per_bin = median(e$w + e$c)
         chrom_sizes = e[, .(xend = max(end)), by = chrom]
         num_bins = nrow(e)
+		y_limit = 2*reads_per_bin+1
     
         # main plot:
         plt <- ggplot(e) +
             aes(x = (start+end)/2) +
             # background rectangles so you see how large the bins are
-            geom_rect(aes(xmin = start, xmax=end, ymin=-2*reads_per_bin, ymax=2*reads_per_bin, fill=rand_bg), inherit.aes=F, alpha=0.2) +
+            #geom_rect(aes(xmin = start, xmax=end, ymin=-2*reads_per_bin, ymax=2*reads_per_bin, fill=rand_bg), inherit.aes=F, alpha=0.2) +
             # Watson/Crick bars
-            geom_bar(aes(y = -w, width=(end-start)), stat='identity', position = 'identity', fill='darkorange') +
-            geom_bar(aes(y = c, width=(end-start)), stat='identity', position = 'identity', fill='dodgerblue3') +
+            geom_bar(aes(y = -w, width=(end-start)), stat='identity', position = 'identity', fill='sandybrown') +
+            geom_bar(aes(y = c, width=(end-start)), stat='identity', position = 'identity', fill='paleturquoise4') +
             # Trim image to 2*median cov
-            coord_flip(expand = F, ylim=c(-2*reads_per_bin,2*reads_per_bin)) +
+            coord_flip(expand = F, ylim=c(-y_limit, y_limit)) +
             facet_grid(.~chrom, switch="x") +
             ylab("Watson | Crick") + xlab(NULL) +
             scale_x_continuous(breaks = pretty_breaks(12), labels = format_Mb) +
@@ -58,13 +59,14 @@ for (s in unique(d$sample))
             guides(fill = FALSE) +
             # Dotted lines at median bin count
             geom_segment(data=chrom_sizes, aes(xend = xend, x=0, y=-reads_per_bin, yend=-reads_per_bin),  linetype="dotted", col="darkgrey") +
-            geom_segment(data=chrom_sizes, aes(xend = xend, x=0, y=+reads_per_bin, yend=+reads_per_bin),  linetype="dotted", col="darkgrey")
+            geom_segment(data=chrom_sizes, aes(xend = xend, x=0, y=+reads_per_bin, yend=+reads_per_bin),  linetype="dotted", col="darkgrey") +
+			geom_segment(data = chrom_sizes, aes(xend = xend, x=0), y=0, yend=0, size=0.5)
     
         # Histogram:
         e.melt <- melt.data.table(e, c("chrom","start","end"), measure.vars = c("w","c"), variable.name = "strand", value.name = "coverage")
         plt_hist <- ggplot(e.melt) + 
             aes(coverage, fill = strand, col = strand, y = ..scaled..) +
-            geom_density(adjust=0.5, alpha=0.3) +
+            geom_density(alpha=0.3) +
             scale_x_continuous(limits = c(0,3*reads_per_bin), breaks = pretty_breaks(4), labels = comma) +
             theme(axis.title.y = element_blank())
     
@@ -76,14 +78,14 @@ for (s in unique(d$sample))
         s_name = substr(s,1,25)
         if (nchar(s)>25) s_name = paste0(s_name, "...")
         
-        all <- ggdraw() + draw_plot(plt) + 
-            draw_plot(plt_hist,    .74, .75, .25,.2) +
+        all <- ggdraw() + draw_plot(plt) +
+		    draw_plot(plt_hist,    .74, .75, .25, .2) +
             draw_plot(plt_covhist, .56, .75, .18, .2) +
-            draw_plot(plt_covhist, .38, .75, .18, .2) +
             draw_label(paste("Sample:", s_name), x=.4, y=.97, vjust=1, hjust=0, size = 14) +
-            draw_label(paste("Avg. binwidth:", format(binwidth, big.mark=",")), x=.4, y=.94, vjust=1, hjust=0, size=10) +
+            draw_label(paste("Median binwidth:", format(round(binwidth/1000,0), big.mark=",", scientific=F),"kb"), x=.4, y=.94, vjust=1, hjust=0, size=10) +
             draw_label(paste("Number bins:", format(num_bins, big.mark=",")), x=.4, y=.92, vjust=1, hjust=0, size=10) +
-            draw_label(paste("Reads/bin:", reads_per_bin), x=.4, y=.90, vjust=1, hjust=0, size=10)
+            draw_label(paste("Median reads/bin (dotted):", reads_per_bin), x=.4, y=.90, vjust=1, hjust=0, size=10) +
+			draw_label(paste0("Plot limits: [-", y_limit, ",", y_limit, "]"), x=.4, y=.88, vjust=1, hjust=0, size=10)
         print(all)
     }
 }
