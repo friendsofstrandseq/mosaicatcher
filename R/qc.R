4e160c5ce518ea025a18fc2eec12dd616c16048b
Sascha Meiers <meiers@embl.de>
2017-05-31 13:17:32 +0200

    Fixed histogram in QC plot + prepared for strand state classification

--- R/qc.R
+++ R/qc.R
@@ -21,6 +21,9 @@ d = d[, chrom := sub('^chr','',chrom)][]
 d = d[grepl('^([1-9]|[12][0-9]|X|Y)$', chrom),]
 d = d[, chrom := factor(chrom, levels=as.character(c(1:22,'X','Y')), ordered = T)]
 
+
+
+
 # Plot all cells
 cairo_pdf(pdf_out, width=14, height=10, onefile = T)
 for (s in unique(d$sample))
@@ -28,25 +31,44 @@ for (s in unique(d$sample))
     for (ce in unique(d[sample == s,]$cell))
     {
         message(paste("Plotting sample", s, "cell", ce,"into",pdf_out))
+
         e = d[sample == s & cell == ce,]
         
-        e$rand_bg = rep(c("a","b"), nrow(e))[1:nrow(e)]
-        binwidth = median(e$end - e$start)
-        reads_per_bin = median(e$w + e$c)
-        chrom_sizes = e[, .(xend = max(end)), by = chrom]
-        num_bins = nrow(e)
-		y_limit = 2*reads_per_bin+1
-    
-        # main plot:
+        # Calculate some information
+        info_binwidth = median(e$end - e$start)
+        info_reads_per_bin = median(e$w + e$c)
+        info_chrom_sizes = e[, .(xend = max(end)), by = chrom]
+        info_num_bins = nrow(e)
+        info_y_limit = 2*info_reads_per_bin+1
+        info_sample_name = substr(s,1,25)
+        if (nchar(s)>25) info_sample_name = paste0(info_sample_name, "...")
+        info_cell_name   = substr(ce,1,25)
+        if (nchar(ce)>25) info_cell_name = paste0(info_cell_name, "...")
+
+        # start main plot:
         plt <- ggplot(e) +
-            aes(x = (start+end)/2) +
-            # background rectangles so you see how large the bins are
-            #geom_rect(aes(xmin = start, xmax=end, ymin=-2*reads_per_bin, ymax=2*reads_per_bin, fill=rand_bg), inherit.aes=F, alpha=0.2) +
-            # Watson/Crick bars
+            aes(x = (start+end)/2)
+
+        # background rectangles
+        if ("class" %in% colnames(e)) {
+
+            # prepare consecutive rectangles for a better plotting experience
+            consecutive = cumsum(abs(diff(as.numeric(as.factor(e$class)))))
+            consecutive = c(consecutive[1], consecutive)
+            e$consecutive = consecutive
+            f = e[, .(start = min(start), end = max(end), class = class[1]), by = .(consecutive, chrom)][]
+
+            plt <- plt +
+                geom_rect(data = f, aes(xmin = start, xmax=end, ymin=-Inf, ymax=Inf, fill=class), inherit.aes=F, alpha=0.2) +
+                scale_fill_manual(values = c(WW = "sandybrown", CC = "paleturquoise4", WC = "yellow", None = NA))
+        }
+
+        # Watson/Crick bars
+        plt <- plt +
             geom_bar(aes(y = -w, width=(end-start)), stat='identity', position = 'identity', fill='sandybrown') +
             geom_bar(aes(y = c, width=(end-start)), stat='identity', position = 'identity', fill='paleturquoise4') +
             # Trim image to 2*median cov
-            coord_flip(expand = F, ylim=c(-y_limit, y_limit)) +
+            coord_flip(expand = F, ylim=c(-info_y_limit, info_y_limit)) +
             facet_grid(.~chrom, switch="x") +
             ylab("Watson | Crick") + xlab(NULL) +
             scale_x_continuous(breaks = pretty_breaks(12), labels = format_Mb) +
@@ -58,34 +80,77 @@ for (s in unique(d$sample))
                   strip.background = element_rect(fill = NA, colour=NA)) + 
             guides(fill = FALSE) +
             # Dotted lines at median bin count
-            geom_segment(data=chrom_sizes, aes(xend = xend, x=0, y=-reads_per_bin, yend=-reads_per_bin),  linetype="dotted", col="darkgrey") +
-            geom_segment(data=chrom_sizes, aes(xend = xend, x=0, y=+reads_per_bin, yend=+reads_per_bin),  linetype="dotted", col="darkgrey") +
-			geom_segment(data = chrom_sizes, aes(xend = xend, x=0), y=0, yend=0, size=0.5)
+            geom_segment(data = info_chrom_sizes, aes(xend = xend, x=0, y=-info_reads_per_bin, yend=-info_reads_per_bin),
+                         linetype="dotted", col="darkgrey", size=0.5) +
+            geom_segment(data = info_chrom_sizes, aes(xend = xend, x=0, y=+info_reads_per_bin, yend=+info_reads_per_bin),
+                         linetype="dotted", col="darkgrey", size=0.5) +
+			geom_segment(data = info_chrom_sizes, aes(xend = xend, x=0), y=0, yend=0, size=0.5)
     
-        # Histogram:
-        e.melt <- melt.data.table(e, c("chrom","start","end"), measure.vars = c("w","c"), variable.name = "strand", value.name = "coverage")
-        plt_hist <- ggplot(e.melt) + 
-            aes(coverage, fill = strand, col = strand, y = ..scaled..) +
-            geom_density(alpha=0.3) +
-            scale_x_continuous(limits = c(0,3*reads_per_bin), breaks = pretty_breaks(4), labels = comma) +
-            theme(axis.title.y = element_blank())
-    
-        # count histogram
-        plt_covhist <- ggplot(e) + aes(w+c) + geom_histogram(bins = 30) +
-            xlab("coverage") + theme(axis.title.y = element_blank()) +
-            scale_x_continuous(limits = c(0, 5*reads_per_bin), label=comma, breaks = pretty_breaks(3))
-        
-        s_name = substr(s,1,25)
-        if (nchar(s)>25) s_name = paste0(s_name, "...")
-        
-        all <- ggdraw() + draw_plot(plt) +
-		    draw_plot(plt_hist,    .74, .75, .25, .2) +
-            draw_plot(plt_covhist, .56, .75, .18, .2) +
-            draw_label(paste("Sample:", s_name), x=.4, y=.97, vjust=1, hjust=0, size = 14) +
-            draw_label(paste("Median binwidth:", format(round(binwidth/1000,0), big.mark=",", scientific=F),"kb"), x=.4, y=.94, vjust=1, hjust=0, size=10) +
-            draw_label(paste("Number bins:", format(num_bins, big.mark=",")), x=.4, y=.92, vjust=1, hjust=0, size=10) +
-            draw_label(paste("Median reads/bin (dotted):", reads_per_bin), x=.4, y=.90, vjust=1, hjust=0, size=10) +
-			draw_label(paste0("Plot limits: [-", y_limit, ",", y_limit, "]"), x=.4, y=.88, vjust=1, hjust=0, size=10)
+
+
+        ###
+        # Behaviour when strand states are given:
+        # Plot a separate histogram for each state.
+        if ("class" %in% colnames(e)) {
+
+            e = merge(e, e[,.N,by=class], by = "class")
+            e[, class := paste0(class, " (n=", N, ")")][]
+            e.melt <- melt.data.table(e, c("chrom","start","end","class"), measure.vars = c("w","c"), variable.name = "strand", value.name = "coverage")
+            plt_hist <- ggplot(e.melt) +
+                aes(coverage, fill = strand) +
+                geom_histogram(binwidth=1) +
+                scale_x_continuous(limits = c(-1, 10+3*info_reads_per_bin), breaks = pretty_breaks(5), labels = comma) +
+                theme(text = element_text(size=10), axis.text = element_text(size=8), axis.title.y = element_blank()) +
+                      #axis.text.y = element_blank(), axis.ticks.y = element_blank()) +
+                scale_fill_manual(values = c(w = "sandybrown", c = "paleturquoise4")) +
+                guides(fill=FALSE) +
+                xlab("bin coverage") + facet_wrap(~class, nrow=1, scales = "free")
+
+
+            all <- ggdraw() + draw_plot(plt) +
+                draw_plot(plt_hist,                            x=.5, y=.76,  width=.49, height=.23) +
+                draw_label(paste("Sample:", info_sample_name), x=.3,  y=.97, vjust=1, hjust=0, size=14) +
+                draw_label(paste("Cell:", info_cell_name),     x=.3,  y=.94, vjust=1, hjust=0, size=12) +
+                draw_label(paste("Median binwidth:", format(round(info_binwidth/1000,0), big.mark=",", scientific=F),"kb"),
+                                                               x=.3,  y=.91, vjust=1, hjust=0, size=10) +
+                draw_label(paste("Number bins:", format(info_num_bins, big.mark=",")),
+                                                               x=.3,  y=.89, vjust=1, hjust=0, size=10) +
+                draw_label(paste("Median reads/bin (dotted):", info_reads_per_bin),
+                                                               x=.3,  y=.87, vjust=1, hjust=0, size=10) +
+                draw_label(paste0("Plot limits: [-", info_y_limit, ",", info_y_limit, "]"),
+                                                               x=.3,  y=.85, vjust=1, hjust=0, size=10)
+
+        ###
+        # Behaviour when strand states are not given:
+        # Plot a coverage histogram and some information
+        } else {
+
+            # Coverage histogram:
+            e.melt <- melt.data.table(e, c("chrom","start","end"), measure.vars = c("w","c"), variable.name = "strand", value.name = "coverage")
+            plt_hist <- ggplot(e.melt) +
+                aes(coverage, fill = strand) +
+                geom_histogram(binwidth=1) +
+                scale_x_continuous(limits = c(-1, 10+3*info_reads_per_bin), breaks = pretty_breaks(5), labels = comma) +
+                theme(text = element_text(size=10), axis.text = element_text(size=8), axis.title.y = element_blank()) +
+                      #axis.text.y = element_blank(), axis.ticks.y = element_blank()) +
+                scale_fill_manual(values = c(w = "sandybrown", c = "paleturquoise4")) +
+                guides(fill=FALSE) +
+                xlab("bin coverage")
+
+
+            all <- ggdraw() + draw_plot(plt) +
+                draw_plot(plt_hist,                            x=.5, y=.76,  width=.18, height=.23) +
+                draw_label(paste("Sample:", info_sample_name), x=.3,  y=.97, vjust=1, hjust=0, size=14) +
+                draw_label(paste("Cell:", info_cell_name),     x=.3,  y=.94, vjust=1, hjust=0, size=12) +
+                draw_label(paste("Median binwidth:", format(round(info_binwidth/1000,0), big.mark=",", scientific=F),"kb"),
+                                                               x=.3,  y=.91, vjust=1, hjust=0, size=10) +
+                draw_label(paste("Number bins:", format(info_num_bins, big.mark=",")),
+                                                               x=.3,  y=.89, vjust=1, hjust=0, size=10) +
+                draw_label(paste("Median reads/bin (dotted):", info_reads_per_bin),
+                                                               x=.3,  y=.87, vjust=1, hjust=0, size=10) +
+                draw_label(paste0("Plot limits: [-", info_y_limit, ",", info_y_limit, "]"),
+                                                               x=.3,  y=.85, vjust=1, hjust=0, size=10)
+        }
         print(all)
     }
 }
