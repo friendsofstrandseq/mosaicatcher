2fcb5c3e11dbd6ceab17a995e04811dbc2ac40f3
Sascha Meiers <meiers@embl.de>
2017-12-11 00:15:19 +0100

    Added git hash to Program version

--- src/main.cpp
+++ src/main.cpp
@@ -30,7 +30,9 @@ int main(int argc, char **argv)
 
     } else if (argc >= 2 && std::string(argv[1]) == "--version") {
         std::cout << "Mosaicatcher " << STRINGIFYMACRO(MOSAIC_VERSION_MAJOR) ;
-        std::cout << "." << STRINGIFYMACRO(MOSAIC_VERSION_MINOR) << std::endl;
+        std::cout << "." << STRINGIFYMACRO(MOSAIC_VERSION_MINOR) << " (";
+		std::cout << STRINGIFYMACRO(MOSAIC_GIT_COMMIT_HASH) << "-";
+        std::cout << STRINGIFYMACRO(MOSAIC_GIT_BRANCH) << ")" << std::endl;
         std::cout << "htslib " << STRINGIFYMACRO(HTSLIB_VERSION) << std::endl;
         std::cout << "boost version " << STRINGIFYMACRO(BOOST_VERSION) << std::endl;
         return 0;
