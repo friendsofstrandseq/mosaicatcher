f77a9139a69da960a76cbf2888b04b41cfca099f
Sascha Meiers <meiers@embl.de>
2018-06-04 08:30:37 +0200

    Revert "Added dummy test"
    This reverts commit 8a91b9244a04158794bae459ae1d28cd7d8403b9.

--- src/tests/segmentation.cpp
+++
@@ -1,7 +0,0 @@
-#define BOOST_TEST_MODULE segmentation test
-#include <boost/test/included/unit_test.hpp>
-#include "../segmentation.hpp"
-
-BOOST_AUTO_TEST_CASE( test_segmentation_lalala ) {
-    BOOST_TEST(true);
-}
